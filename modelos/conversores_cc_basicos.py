bf7b52be98684b7e8db5d0161817d58d456abea6
mariobigai <mariobigai1@gmail.com>
2023-04-02 13:38:23 -0300

    Implementando forced_response_TS e testando na main

--- modelos/conversores_cc_basicos.py
+++ modelos/conversores_cc_basicos.py
@@ -1,4 +1,28 @@
-from auxiliar import *
+import control as ctr
+from modelos import auxiliar
+import numpy as np
+import scipy as sp
+
+
+## Função para somar matrizes dinâmicamente no modelo TS
+## Provavelmente tenha isso otimizado no Numpy, verificar e substituir depois
+def somar(A, B):
+    C = []
+    # verificar se A e B tem a mesma ordem
+    nLinhasA, nLinhasB = len(A), len(B)
+    nColA, nColB = len(A[0]), len(B[0])
+    if (nLinhasA == nLinhasB) and (nColA == nColB):
+        # posso somar
+        for i in range(nLinhasA):
+            linha = [0] * nColA
+            C.append(linha)
+            for j in range(nColA):
+                C[i][j] = A[i][j] + B[i][j]
+    else:
+        print("Matrizes não tem a mesma ordem")
+
+    return C
+
 class Buck_Boost_ideal:
     def __init__(self, R, C, L, Vin):
         self._R = R
@@ -6,32 +30,56 @@ class Buck_Boost_ideal:
         self._L = L
         self._Vin = Vin
 
-    ## Função para somar matrizes dinâmicamente no modelo TS
-    ## Provavelmente tenha isso otimizado no Numpy, verificar e substituir depois
-    def somar(A, B):
-        C = []
-        # verificar se A e B tem a mesma ordem
-        nLinhasA, nLinhasB = len(A), len(B)
-        nColA, nColB = len(A[0]), len(B[0])
-        if (nLinhasA == nLinhasB) and (nColA == nColB):
-            # posso somar
-            for i in range(nLinhasA):
-                linha = [0] * nColA
-                C.append(linha)
-                for j in range(nColA):
-                    C[i][j] = A[i][j] + B[i][j]
-        else:
-            print("Matrizes não tem a mesma ordem")
+    def forced_response_TS(self, d_t, T=None, U=0., X0=0., transpose=False, interpolate=False, return_x=None, squeeze=None):
 
-    return C
+        """
+        Método para gerar a resposta em malha aberta do modelo TS do Buck-Boost operando em várias regiões de operação
 
-    def cria_SS:
-        pass
+        :param D_t: Regiões de opeação (Duty-cycle)
+        :param T: Tempo de simulação
+        :param U: Entrada do sistema
+        :param X0: (Biblioteca control)
+        :param transpose: (Biblioteca control)
+        :param interpolate: (Biblioteca control)
+        :param return_x: (Biblioteca control)
+        :param squeeze: (Biblioteca control)
+        :return: Resultado da simulação: x, y
+        """
+        # Região mínima de operação
+        Il_min = (self._Vin / self._R) * (d_t[0] / (1 - d_t[0]))
+        Vc_min = self._Vin * (d_t[0] / (1-d_t[0]))
 
-    def forced_response_TS(sys, T=None, U=0., X0=0., transpose=False, interpolate=False, return_x=None, squeeze=None):
+        # Região máxima de operação
+        Il_max = (self._Vin / self._R) * (d_t[-1] / (1 - d_t[-1]))
+        Vc_max = self._Vin * (d_t[-1] / (1-d_t[-1]))
+
+        # Máximos e mínimos de g11
+        b111 = (self._Vin + Vc_max) / self._L
+        b112 = (self._Vin + Vc_min) / self._L
+
+        # Máximos e mínimos de g12
+        b211 = (-1 * Il_min) / self._C
+        b212 = (-1 * Il_max) / self._C
+
+        # Modelos lineares locais
+        B1 = np.asarray([[b111], [b211]])
+        B2 = np.asarray([[b112], [b211]])
+        B3 = np.asarray([[b111], [b212]])
+        B4 = np.asarray([[b112], [b212]])
+
+        # Considerando o inicialmente aplha1 = 1, alpha2,3,4 =0
+        A_TS = [[0, -1/self._L],
+                [1/self._C, -1/(self._R*self._C)]]
+        B_TS = somar(0 * B4, somar(0 * B3, somar(1 * B1, 0 * B2)))
+        C_TS = [[0,1],[1,0]]
+        E_TS = [[0],[0]]
+        SS_TS = ctr.StateSpace(A_TS, B_TS, C_TS, E_TS)
 
         # Pega as matrizes de estado
-        A, B, C, D = np.asarray(sys.A), np.asarray(sys.B), np.asarray(sys.C), np.asarray(sys.D)
+        A, B, C, E = np.asarray(SS_TS.A), np.asarray(SS_TS.B), np.asarray(SS_TS.C), np.asarray(SS_TS.D)
+
+        ## ------------------------------------------------------------------------------------------------------------
+        # Verificações
 
         #Numero de estados
         n_states = A.shape[0]
@@ -48,13 +96,19 @@ class Buck_Boost_ideal:
         if T is not None:
             T = np.asarray(T) # cria array de tempo
 
+        #Garante que U e T tem o mesmo tamanho
+        if (U.ndim == 1 and U.shape[0] != T.shape[0]) or \
+                (U.ndim > 1 and U.shape[1] != T.shape[0]):
+            ValueError('Pamameter ``T`` must have same elements as'
+                       ' the number of columns in input array ``U``')
+
         # Erro se T é nulo
         if T is None:
             raise ValueError('Parameter ``T``: must be array-like, and contain '
                              '(strictly monotonic) increasing numbers.')
 
         # Verifica e atribui o T no formato correto
-        T = _check_convert_array(T, [('any',), (1, 'any')], 'Parameter ``T``: ', squeeze=True, transpose=transpose)
+        T = auxiliar._check_convert_array(T, [('any',), (1, 'any')], 'Parameter ``T``: ', squeeze=True, transpose=transpose)
 
         dt = T[1] - T[0] #Passo de simulação
 
@@ -66,17 +120,17 @@ class Buck_Boost_ideal:
         n_steps = T.shape[0]  # número de passos de simulação
 
         # Cria vetor X0 se não for dado e testa se está no formato correto
-        X0 = _check_convert_array(X0, [(n_states,), (n_states, 1)], 'Parameter ``X0``: ', squeeze=True)
+        X0 = auxiliar._check_convert_array(X0, [(n_states,), (n_states, 1)], 'Parameter ``X0``: ', squeeze=True)
 
         xout = np.zeros((n_states, n_steps))
         xout[:, 0] = X0
         yout = np.zeros((n_outputs, n_steps))
 
-        ## Considerando apenas o caso contínuo
+        ## Considerando apenas o caso contínuo com U diferente de zero
         legal_shapes = [(n_steps,), (1, n_steps)] if n_inputs == 1 else \
-            [(n_inputs, n_steps)]
+                       [(n_inputs, n_steps)]
 
-        U = _check_convert_array(U, legal_shapes,
+        U = auxiliar._check_convert_array(U, legal_shapes,
                                  'Parameter ``U``: ', squeeze=False,
                                  transpose=transpose)
 
@@ -92,15 +146,66 @@ class Buck_Boost_ideal:
 
         # Laço principal
         for i in range(1, n_steps):
-            pass
-        xout = 0
-        yout = 0
+            # divide o tempo de simulação em 9 partes para percorrer o vetor d_t
+            if i < n_steps * 0.1:
+                aux = 0
+            elif i == n_steps * 0.1:
+                aux = 1
+            elif i == n_steps * 0.2:
+                aux = 2
+            elif i == n_steps * 0.3:
+                aux = 3
+            elif i == n_steps * 0.4:
+                aux = 4
+            elif i == n_steps * 0.5:
+                aux = 5
+            elif i == n_steps * 0.6:
+                aux = 6
+            elif i == n_steps * 0.7:
+                aux = 7
+            elif i == n_steps * 0.8:
+                aux = 8
+            elif i == n_steps * 0.9:
+                aux = 9
 
-        tout = T
+            Il = (self._Vin / self._R) * (d_t[aux] / (1 - d_t[aux]))
+            Vc = self._Vin * (d_t[aux] / (1-d_t[aux]))
 
-        xout = np.transpose(xout)
-        yout = np.transpose(yout)
-    return xout, yout
+            g11 = (self._Vin + Vc)/self._L
+            g21 = -Vc/self._C
+
+            sig111 = (g11 - b112) / (b111 - b112)
+            sig112 = (b111 - g11) / (b111 - b112)
+            eps211 = (g21 - b212) / (b211 - b212)
+            eps212 = (b211 - g21) / (b211 - b212)
+
+            alpha1 = eps211 * sig111
+            alpha2 = eps211 * sig112
+            alpha3 = eps212 * sig111
+            alpha4 = eps212 * sig112
+
+            B = np.asarray(somar(alpha4 * B4, somar(alpha3 * B3, somar(alpha1 * B1, alpha2 * B2))))
+
+            U = d_t[aux] * np.ones_like(T)
+            if U is not None:
+                U = np.asarray(U)
+            if len(U.shape) == 1:
+                U = U.reshape(1, -1)
+
+            M = np.block([[A * dt, B * dt, np.zeros((n_states, n_inputs))],
+                          [np.zeros((n_inputs, n_states + n_inputs)),
+                           np.identity(n_inputs)],
+                          [np.zeros((n_inputs, n_states + 2 * n_inputs))]])
+            expM = sp.linalg.expm(M)
+            Ad = expM[:n_states, :n_states]
+            Bd1 = expM[:n_states, n_states + n_inputs:]
+            Bd0 = expM[:n_states, n_states:n_states + n_inputs] - Bd1
+
+            xout[:, i] = (np.dot(Ad, xout[:, i - 1]) + np.dot(Bd0, U[:, i - 1]) +
+                          np.dot(Bd1, U[:, i]))
+            yout = np.dot(C, xout) + np.dot(E, U)
+
+        return yout
 
 
 
--- modelos/main.py
+++ modelos/main.py
@@ -2,8 +2,28 @@
 Arquivo main para testar códigos dos modelos dos conversores
 Trabalho Futuro - Refatorar implementando TDD
 """
-
+import matplotlib.pyplot as plt
 import conversores_cc_basicos
+import numpy as np
 
 conversor = conversores_cc_basicos.Buck_Boost_ideal(1.96, 375e-6, 0.146e-3, 24)
-print(conversor)
+duty = list(np.arange(0,1,0.1))
+T = np.arange(0,0.1,0.0001)
+U = np.ones_like(T)
+ys_TS = conversor.forced_response_TS(duty, T, U, 0., False, False, None, True)
+
+print(ys_TS.shape)
+#---------------------------------------------------------------------------------
+# Plotando o resultado
+fig1, f1_axes = plt.subplots(ncols=1, nrows=2, figsize=(15,10))
+f1_axes[0].plot(T, ys_TS[0], linewidth=1.5, label="Modelo fuzzy TS")
+f1_axes[0].set_title("Tensã de Saída (V)")
+f1_axes[0].set_xlabel("Tempo (s)")
+f1_axes[0].set_ylabel("Tensão (V)")
+f1_axes[0].legend()
+f1_axes[1].plot(T, ys_TS[1], linewidth=1.5, label="Modelo fuzzy TS")
+f1_axes[1].set_title("Corrente de Saída (A)")
+f1_axes[1].set_xlabel("Tempo (s)")
+f1_axes[1].set_ylabel("Corrente (A)")
+f1_axes[1].legend()
+plt.show()
